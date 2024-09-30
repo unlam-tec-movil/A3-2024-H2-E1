8832d1eaac391c5bf3b45268c7b6f40d95d85424
Giuli <gmenicucci221@alumno.unlam.edu.ar>
2024-09-30 17:41:20 -0300

    Se creó el feature itemProducto

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/MainActivity.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/MainActivity.kt
@@ -42,27 +42,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    // Controller es el elemento que nos permite navegar entre pantallas. Tiene las acciones
-    // para navegar como naviegate y también la información de en dónde se "encuentra" el usuario
-    // a través del back stack
     val controller = rememberNavController()
     Scaffold(
-        bottomBar = { BottomBar(controller = controller) },
+        bottomBar = { BottomBar(controller = controller) }, // Si tienes una barra de navegación
         floatingActionButton = {
-            IconButton(onClick = { controller.navigate("home") }) {
+            IconButton(onClick = { controller.navigate("home") }) { // Navegación a HomeScreen
                 Icon(Icons.Filled.Home, contentDescription = "Home")
             }
         },
     ) { paddingValue ->
-        // NavHost es el componente que funciona como contenedor de los otros componentes que
-        // podrán ser destinos de navegación.
         NavHost(navController = controller, startDestination = "home") {
-            // composable es el componente que se usa para definir un destino de navegación.
-            // Por parámetro recibe la ruta que se utilizará para navegar a dicho destino.
             composable("home") {
-                // Home es el componente en sí que es el destino de navegación.
-                HomeScreen(modifier = Modifier.padding(paddingValue))
+                HomeScreen(modifier = Modifier.padding(paddingValue)) // Tu pantalla principal
             }
+            // Si necesitas otras pantallas en el futuro, puedes agregarlas aquí
         }
     }
 }
