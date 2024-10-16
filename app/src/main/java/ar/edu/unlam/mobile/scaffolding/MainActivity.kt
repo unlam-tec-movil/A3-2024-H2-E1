d21a939eeeae2e1526e6798cb6c151b625bb042b
Tomás Agustín Leggire <tleggire@alumno.unlam.edu.ar>
2024-10-16 18:58:49 -0300

    Comienzo de maquetado de home screen

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/MainActivity.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/MainActivity.kt
@@ -18,7 +18,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import ar.edu.unlam.mobile.scaffolding.ui.components.BottomBar
-import ar.edu.unlam.mobile.scaffolding.ui.components.CategoryList
 import ar.edu.unlam.mobile.scaffolding.ui.screens.HomeScreen
 import ar.edu.unlam.mobile.scaffolding.ui.theme.ScaffoldingV2Theme
 import dagger.hilt.android.AndroidEntryPoint
@@ -63,7 +62,6 @@ fun MainScreen() {
             composable("home") {
                 // Home es el componente en sí que es el destino de navegación.
                 HomeScreen(modifier = Modifier.padding(paddingValue))
-                CategoryList()
             }
         }
     }
--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/HomeScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/HomeScreen.kt
@@ -1,11 +1,27 @@
 package ar.edu.unlam.mobile.scaffolding.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import ar.edu.unlam.mobile.scaffolding.ui.components.Greeting
+import ar.edu.unlam.mobile.scaffolding.R
 
 @Composable
 fun HomeScreen(
@@ -23,11 +39,39 @@ fun HomeScreen(
         }
 
         is HelloMessageUIState.Success -> {
-            Greeting(helloState.message, modifier)
+            HomeScreenUI(modifier)
         }
 
         is HelloMessageUIState.Error -> {
             // Error
+        }
+    }
+}
+
+@Composable
+fun HomeScreenUI(modifier: Modifier) {
+    Column(modifier.fillMaxSize().padding(12.dp)) {
+        Row(modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = painterResource(id = R.drawable.location_icon),
+                contentDescription = "Location Icon",
+                modifier =
+                    Modifier
+                        .size(35.dp)
+                        .clip(CircleShape),
+            )
+            Text(
+                text = "Gaona 2340, Ramos Mejia",
+                textDecoration = TextDecoration.Underline,
+                modifier = Modifier.padding(start = 4.dp),
+                fontSize = 16.sp,
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Image(
+                painter = painterResource(id = R.drawable.qr_scan_icon),
+                contentDescription = "QR Scan Icon",
+                modifier = Modifier.size(35.dp),
+            )
         }
     }
 }
