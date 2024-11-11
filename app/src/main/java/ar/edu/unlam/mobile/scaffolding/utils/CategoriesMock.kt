213f43c0324113ee8e040784ea7ce7748232f711
Nicolás Cartellone <ncartellone@alumno.unlam.edu.ar>
2024-11-11 20:33:36 -0300

    fix ktlint

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/utils/CategoriesMock.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/utils/CategoriesMock.kt
@@ -14,45 +14,62 @@ object CategoriesMock {
             Category(
                 id = 1,
                 name = "Milanesas",
-                image = @Suppress("ktlint:standard:max-line-length")
-                "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/7fd9e77b-3396-49f3-be08-fceef56376bf/Derivates/8a096b0f-382a-47ac-b11a-62b630e0e59e.jpg",
+                image =
+                    @Suppress("ktlint:standard:max-line-length")
+                    "https://assets.tmecosys.com/image/upload/t_web767x639/img/"
+                        +
+                        "recipe/ras/Assets/" +
+                        "7fd9e77b-3396-49f3-be08-fceef56376bf/Derivates" +
+                        "/8a096b0f-382a-47ac-b11a-62b630e0e59e.jpg",
                 selected = false,
             ),
             Category(
                 id = 2,
                 name = "Pizzas",
-                image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI2hdQeNVlyu20ReOpJcNwdgW0ER5hwxnauQ&s",
+                image =
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI2hdQeNVlyu" +
+                        "20ReOpJcNwdgW0ER5hwxnauQ&s",
                 selected = false,
             ),
             Category(
                 id = 3,
                 name = "Pastas",
-                image = "https://conzazoni.com/wp-content/uploads/2023/05/recetas-especiales-con-pastas-conzazoni.webp",
+                image =
+                    "https://conzazoni.com/wp-content/uploads/2023/05/recetas-" +
+                        "especiales-con-pastas-conzazoni.webp",
                 selected = false,
             ),
             Category(
                 id = 4,
                 name = "Postres",
-                image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRYvCPseOQTqpTWGJNCwmb4ZXQ-T-coeOHfQ&s",
+                image =
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:" +
+                        "ANd9GcRRYvCPseOQTqpTWGJNCwmb4ZXQ-T-coeOHfQ&s",
                 selected = false,
             ),
             Category(
                 id = 5,
                 name = "Bebidas",
-                image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUpraU7RJhGhyvtTIKsxlpj2j9sz0tuLlwVg&s",
+                image =
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:" +
+                        "ANd9GcRUpraU7RJhGhyvtTIKsxlpj2j9sz0tuLlwVg&s",
                 selected = false,
             ),
             Category(
                 id = 6,
                 name = "Carnes",
-                image = "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/XTWCVOJ5LZCTFDSMDYIVGMDXCU.jpg",
+                image =
+                    "https://cloudfront-us-east-1.images.arcpublishing.com/" +
+                        "infobae/XTWCVOJ5LZCTFDSMDYIVGMDXCU.jpg",
                 selected = false,
             ),
             Category(
                 id = 7,
                 name = "Empanadas",
-                image = @Suppress("ktlint:standard:max-line-length")
-                "https://www.recetasnatura.com.ar/sites/default/files/styles/receta_cuerpo/public/655_x_475-_web_naturaempanadas_de_roastbeef_desmenuzado.jpg?itok=8iHtVcq3",
+                image =
+                    "https://www.recetasnatura.com.ar/sites/default/files/styles/receta_cuerpo/" +
+                        "public/655_x_475-_" +
+                        "web_naturaempanadas_de_roastbeef_desmenuzado.jpg?itok=8iHtVcq3",
                 selected = false,
             ),
         )
--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/utils/ProductsMocks.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/utils/ProductsMocks.kt
@@ -10,7 +10,8 @@ object ProductsMocks {
                 "Asado",
                 "Carne de res asada a la parrilla, con acompañamiento de ensaladas, papas y pan",
                 18.50,
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3QjzVgP1LiWdTQKVa_nmlMOEjOTzBKFNhrQ&s",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3QjzVg" +
+                    "P1LiWdTQKVa_nmlMOEjOTzBKFNhrQ&s",
                 "Platos principales",
             ),
             Product(
@@ -18,7 +19,8 @@ object ProductsMocks {
                 "Empanadas de Carne",
                 "Empanadas rellenas de carne picada, cebolla, huevo duro y aceitunas",
                 3.00,
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt3r_y3njok-fPs5poiMYRuz-zi2jDANEncg&s",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt3r_" +
+                    "y3njok-fPs5poiMYRuz-zi2jDANEncg&s",
                 "Entrante",
             ),
             Product(
@@ -26,7 +28,10 @@ object ProductsMocks {
                 "Milanesa a la Napolitana",
                 "Milanesa de carne empanada, cubierta con jamón, queso y salsa de tomate",
                 10.00,
-                "https://resizer.glanacion.com/resizer/v2/milanesa-a-la-napolitana-con-guarnicion-de-papas-VLWFAANIWBGPFO4CSUHS7RYVVQ.jpg?auth=335fda04cf2733e39d11ca0ba979c1d0a8a55e6cdec15e4d5b00cfd59fbf9ed8&width=1280&height=854&quality=70&smart=true",
+                "https://resizer.glanacion.com/resizer/v2/milanesa-a-la-napolitana-" +
+                    "con-guarnicion-de-papas-VLWFAANIWBGPFO4CSUHS7RYVVQ.jpg?auth=335" +
+                    "fda04cf2733e39d11ca0ba979c1d0a8a55e6cdec15e4d5b00cfd59fbf9ed8&" +
+                    "width=1280&height=854&quality=70&smart=true",
                 "Platos principales",
             ),
             Product(
@@ -34,7 +39,8 @@ object ProductsMocks {
                 "Provoleta",
                 "Queso provolone a la parrilla, acompañado de orégano y ají molido",
                 7.00,
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLkbR2VmX_ePDwXDrTjWdyhL_tmf5WVHWNFg&s",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLkbR2Vm" +
+                    "X_ePDwXDrTjWdyhL_tmf5WVHWNFg&s",
                 "Entrante",
             ),
             Product(
@@ -50,7 +56,8 @@ object ProductsMocks {
                 "Empanadas de Jamón y Queso",
                 "Empanadas rellenas con jamón y queso fundido",
                 3.50,
-                "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/NYWX2P3TGNFMZPNLMWOR3QNQDQ.jpg",
+                "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/" +
+                    "NYWX2P3TGNFMZPNLMWOR3QNQDQ.jpg",
                 "Entrante",
             ),
             Product(
@@ -58,7 +65,8 @@ object ProductsMocks {
                 "Bife de Chorizo",
                 "Bife de chorizo a la parrilla, jugoso y tierno, acompañado de papas fritas",
                 16.00,
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbbdb36Ei_UIg6KdVwRePFu-mVko4fKS6NvQ&s",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbbdb36Ei_" +
+                    "UIg6KdVwRePFu-mVko4fKS6NvQ&s",
                 "Platos principales",
             ),
             Product(
@@ -66,7 +74,8 @@ object ProductsMocks {
                 "Fugazza",
                 "Pizza argentina con cebolla caramelizada, aceitunas y orégano",
                 9.50,
-                "https://assets.elgourmet.com/wp-content/uploads/2023/03/fugaz_9AZICtTMHNiBKq3vVgj6Qz4hD1F0c2-1024x683.png.webp",
+                "https://assets.elgourmet.com/wp-content/uploads/2023/03/fugaz_" +
+                    "9AZICtTMHNiBKq3vVgj6Qz4hD1F0c2-1024x683.png.webp",
                 "Platos principales",
             ),
             Product(
@@ -74,13 +83,17 @@ object ProductsMocks {
                 "Faina",
                 "Pan de garbanzo que se sirve tradicionalmente con pizza",
                 4.00,
-                "https://www.infobae.com/resizer/v2/YEMJ6DEIGZDMHIIFPXEKBJEABU.jpg?auth=46ed990025ff2d9faa4acfc4c6263f1a2330116ea4cd63f0c523d2ee28a89094&smart=true&width=350&height=197&quality=85",
+                "https://www.infobae.com/resizer/v2/YEMJ6DEIGZDMHIIFPXEKBJEABU." +
+                    "jpg?auth=46ed990025ff2d9faa4acfc4c6263f1a2330116ea4cd63f0c523" +
+                    "d2ee28a89094&smart=true&width=350&height=197&quality=85",
                 "Entrante",
             ),
             Product(
                 10,
                 "Matambre a la Pizza",
-                "Matambre de cerdo o res, a la parrilla con salsa de tomate, mozzarella y orégano",
+                "Matambre de cerdo o res, a la parrilla con salsa de " +
+                    "tomate, mozzarella " +
+                    "y orégano",
                 14.00,
                 "https://www.clarin.com/2022/11/25/tR-l3EmRl_2000x1500__1.jpg",
                 "Platos principales",
@@ -88,9 +101,11 @@ object ProductsMocks {
             Product(
                 11,
                 "Milanesa de Pollo",
-                "Milanesa de pollo empanada y frita, servida con papas fritas o ensalada",
+                "Milanesa de pollo empanada y frita, servida con papas " +
+                    "fritas o ensalada",
                 10.00,
-                "https://alicante.com.ar/wp-content/uploads/2022/06/receta-milanesa-pollo-alicante-scaled-1707x1707.jpg",
+                "https://alicante.com.ar/wp-content/uploads/2022/06/receta-" +
+                    "milanesa-pollo-alicante-scaled-1707x1707.jpg",
                 "Platos principales",
             ),
             Product(
@@ -98,7 +113,8 @@ object ProductsMocks {
                 "Tarta de Verdura",
                 "Tarta salada rellena de acelga, cebolla, huevo y queso",
                 7.00,
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ01b-qIOWsfWOpFvmy6UZSSTgZ2LDiYf6YNg&s",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ01b-" +
+                    "qIOWsfWOpFvmy6UZSSTgZ2LDiYf6YNg&s",
                 "Entrante",
             ),
             Product(
@@ -106,7 +122,8 @@ object ProductsMocks {
                 "Papas Fritas",
                 "Papas fritas caseras, crujientes por fuera y blandas por dentro",
                 3.50,
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC66b_fMsyU19Un2kJ3WKDT2TjdCwK7r1giw&s",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC66b_" +
+                    "fMsyU19Un2kJ3WKDT2TjdCwK7r1giw&s",
                 "Acompañamientos",
             ),
             Product(
@@ -114,7 +131,8 @@ object ProductsMocks {
                 "Pastelito de Dulce de Membrillo",
                 "Masa frita rellena con dulce de membrillo",
                 2.50,
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDJFklnuhSqj4gZH8hAidDOWlUPa_NdgkANw&s",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDJFklnuh" +
+                    "Sqj4gZH8hAidDOWlUPa_NdgkANw&s",
                 "Postres",
             ),
             Product(
@@ -122,29 +140,35 @@ object ProductsMocks {
                 "Chipa",
                 "Panecillo de queso tradicional de la región del noreste argentino",
                 1.50,
-                "https://img-global.cpcdn.com/recipes/9c9c7c0579ce0792/1200x630cq70/photo.jpg",
+                "https://img-global.cpcdn.com/recipes/9c9c7c0579ce0792/1200x630" +
+                    "q70/photo.jpg",
                 "Entrante",
             ),
             Product(
                 16,
                 "Locro",
-                "Sopa espesa de maíz, zapallo, carne de cerdo y vaca, ideal para el 25 de mayo",
+                "Sopa espesa de maíz, zapallo, carne de cerdo y vaca, i" +
+                    "deal para el 25 de mayo",
                 12.00,
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDmnKuQAoZNS_c3xno1ToF3VXlQFzurhkadQ&s",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDmnKuQA" +
+                    "oZNS_c3xno1ToF3VXlQFzurhkadQ&s",
                 "Platos principales",
             ),
             Product(
                 17,
                 "Ensalada Criolla",
-                "Ensalada fresca con tomate, cebolla, pimiento y aderezo de aceite y vinagre",
+                "Ensalada fresca con tomate, cebolla, pimiento y " +
+                    "aderezo de aceite y vinagre",
                 4.00,
-                "https://img-global.cpcdn.com/recipes/recipes_5153_v1405233731_foto_foto_00141761-he6navjemyijmhrfqgc2/400x400cq70/photo.jpg",
+                "https://img-global.cpcdn.com/recipes/recipes_5153_v1405233731_" +
+                    "foto_foto_00141761-he6navjemyijmhrfqgc2/400x400cq70/photo.jpg",
                 "Entrante",
             ),
             Product(
                 18,
                 "Milanesa de Soja",
-                "Milanesa vegetariana hecha a base de soja, acompañada de arroz o ensalada",
+                "Milanesa vegetariana hecha a base de soja, acompañada " +
+                    "de arroz o ensalada",
                 9.50,
                 "https://alicante.com.ar/wp-content/uploads/2022/06/231_receta.jpg",
                 "Platos principales",
@@ -152,9 +176,11 @@ object ProductsMocks {
             Product(
                 19,
                 "Canelones de Ricota",
-                "Pasta rellena con ricota y espinaca, bañada en salsa de tomate y bechamel",
+                "Pasta rellena con ricota y espinaca, bañada en salsa de " +
+                    "tomate y bechamel",
                 11.50,
-                "https://t2.uc.ltmcdn.com/es/posts/6/8/8/como_hacer_canelones_de_ricota_36886_orig.jpg",
+                "https://t2.uc.ltmcdn.com/es/posts/6/8/8/como_hacer_canelones" +
+                    "_de_ricota_36886_orig.jpg",
                 "Platos principales",
             ),
             Product(
@@ -178,7 +204,8 @@ object ProductsMocks {
                 "Tarta de Carne",
                 "Tarta rellena con carne picada, cebolla y huevo duro",
                 8.00,
-                "https://img-global.cpcdn.com/recipes/212bf10bd52132b5/1200x630cq70/photo.jpg",
+                "https://img-global.cpcdn.com/recipes/212bf10bd52132b5/1200x63" +
+                    "0cq70/photo.jpg",
                 "Entrante",
             ),
             Product(
@@ -186,7 +213,8 @@ object ProductsMocks {
                 "Cordero Patagónico",
                 "Cordero asado a la parrilla, típico de la región sur de Argentina",
                 22.00,
-                "https://www.skorpios.cl/wp-content/uploads/El-cordero-patag%C3%B3nico-de-Skorpios.jpg",
+                "https://www.skorpios.cl/wp-content/uploads/El-cordero-patag%C3%" +
+                    "B3nico-de-Skorpios.jpg",
                 "Platos principales",
             ),
             Product(
@@ -194,15 +222,18 @@ object ProductsMocks {
                 "Papas a la Provenzal",
                 "Papas fritas al estilo provenzal, con ajo y perejil",
                 4.50,
-                "https://truffle-assets.tastemadecontent.net/58346f15-papas-a-la-provenzal_l_thumb.jpg",
+                "https://truffle-assets.tastemadecontent.net/58346f15-papas-a-la" +
+                    "-provenzal_l_thumb.jpg",
                 "Acompañamientos",
             ),
             Product(
                 26,
                 "Helado",
-                "Helado artesanal con sabores típicos como dulce de leche, chocolate amargo y frutilla",
+                "Helado artesanal con sabores típicos como dulce de leche, c" +
+                    "hocolate amargo y frutilla",
                 4.50,
-                "https://frutto.com.ar/wp-content/uploads/2022/11/helado-de-chocolate-para-veganos-Lomas-de-Zamora.jpg",
+                "https://frutto.com.ar/wp-content/uploads/2022/11/helado-de-ch" +
+                    "ocolate-para-veganos-Lomas-de-Zamora.jpg",
                 "Postres",
             ),
             Product(
@@ -226,7 +257,9 @@ object ProductsMocks {
                 "Milanesa a la Suiza",
                 "Milanesa empanada con una capa de queso suizo derretido",
                 11.00,
-                "https://www.paulinacocina.net/wp-content/uploads/2024/05/milanesa-de-ternera-a-la-suiza-paulina-cocina-recetas-1722444278-1200x675.jpg",
+                "https://www.paulinacocina.net/wp-content/uploads/2024/05/mi" +
+                    "lanesa-de-ternera-a-la-suiza-paulina-cocina-recetas-1722" +
+                    "444278-1200x675.jpg",
                 "Platos principales",
             ),
             Product(
@@ -234,7 +267,8 @@ object ProductsMocks {
                 "Fernet con Coca",
                 "Cóctel clásico argentino hecho con Fernet y Coca-Cola",
                 3.00,
-                "https://atencion24.ar/wp-content/uploads/2023/10/24d7d091-6735-4053-a6be-e90442b53949.jpg",
+                "https://atencion24.ar/wp-content/uploads/2023/10/24d7d091-6735" +
+                    "-4053-a6be-e90442b53949.jpg",
                 "Bebidas",
             ),
         )
