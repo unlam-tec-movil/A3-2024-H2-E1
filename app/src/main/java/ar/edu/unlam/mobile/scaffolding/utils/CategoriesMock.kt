8c531429d06a7592e977915f5b5365414c423dd9
Nicolás Cartellone <ncartellone@alumno.unlam.edu.ar>
2024-11-11 20:34:53 -0300

    fix

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/utils/CategoriesMock.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/utils/CategoriesMock.kt
@@ -16,8 +16,7 @@ object CategoriesMock {
                 name = "Milanesas",
                 image =
                     @Suppress("ktlint:standard:max-line-length")
-                    "https://assets.tmecosys.com/image/upload/t_web767x639/img/"
-                        +
+                    "https://assets.tmecosys.com/image/upload/t_web767x639/img/"+
                         "recipe/ras/Assets/" +
                         "7fd9e77b-3396-49f3-be08-fceef56376bf/Derivates" +
                         "/8a096b0f-382a-47ac-b11a-62b630e0e59e.jpg",
